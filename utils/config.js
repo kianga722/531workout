5d7dc6621a9991b29991e88a7cbad3b9712a8072
kianga722 <kianga722@gmail.com>
2019-10-05 22:32:04 -0400

    edited config for running database locally and eslint

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 let { MONGODB_URI } = process.env;
 
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV !== 'production') {
   MONGODB_URI = process.env.TEST_MONGODB_URI;
 }
 
